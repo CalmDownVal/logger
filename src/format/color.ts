be4c630b24b5dc3d1c78398d316455ce2d80284a
CalmDownVal <vaclav.lahuta@gmail.com>
2020-12-12 14:22:01 +0100

    unify log level formatting

--- src/format/color.ts
+++ src/format/color.ts
@@ -6,8 +6,8 @@ const gray = c.fg(0x808080);
 export const colorFormatter = createFormatter({
 	logLevelMap: {
 		[LogLevel.Debug]:   ` ${c.fg(0x40ff00)}DEBUG${c.reset} `,
-		[LogLevel.Info]:    ` ${c.fg(0x0080ff)}INFO${c.reset}  `,
-		[LogLevel.Warning]: ` ${c.fg(0xffff00)}WARN${c.reset}  `,
+		[LogLevel.Info]:    ` ${c.fg(0x0080ff)}INFO${c.reset} `,
+		[LogLevel.Warning]: ` ${c.fg(0xffff00)}WARN${c.reset} `,
 		[LogLevel.Error]:   ` ${c.fg(0xff0000)}ERROR${c.reset} `
 	},
 	tagSeparator: `${gray}][${c.reset}`,
--- src/format/default.ts
+++ src/format/default.ts
@@ -4,8 +4,8 @@ import { LogLevel } from '../LogLevel';
 export const defaultFormatter = createFormatter({
 	logLevelMap: {
 		[LogLevel.Debug]:   'DEBUG ',
-		[LogLevel.Info]:    'INFO  ',
-		[LogLevel.Warning]: 'WARN  ',
+		[LogLevel.Info]:    'INFO ',
+		[LogLevel.Warning]: 'WARN ',
 		[LogLevel.Error]:   'ERROR '
 	},
 	tagSeparator: '][',
